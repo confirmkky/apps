159cfed8e98932217f586df7f300e1a38f861efe
confirmkky <kunal8228866191@gmail.com>
2023-12-23 19:28:27 +0530

    Update

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
- DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation
